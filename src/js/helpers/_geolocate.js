bce869da20d260fb28b9f07bb492ca9a86fe3cdb
Michael Weaver <mjweaver01@gmail.com>
2022-05-17 16:21:59 -0400

    Fix geolocate by using custom netlify function

--- src/js/helpers/_geolocate.js
+++ src/js/helpers/_geolocate.js
@@ -3,7 +3,7 @@ export default async function geolocate() {
   const defaultCode = 'US'
 
   if (!geocode) {
-    const data = await fetch('https://freegeoip.app/json/').then(res => res.json())
+    const data = await fetch('https://ee-ip.netlify.app/.netlify/functions/geoip').then(res => res.json())
     const code = data.country_code ? data.country_code : defaultCode
 
     localStorage.setItem('geocode', code)
